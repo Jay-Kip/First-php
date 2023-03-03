d1d923d33e8af5a1646639b5073382285e4b284b
Jays <hanningtonkiptoo14@gmail.com>
2023-03-03 11:27:20 +0300

    First php commit

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+    
+        
+        
+        <?php
+        echo("<h1>Jay's site</h1>");
+        echo("<hr>");
+        echo("<P>This is my site</P>");
+        
+        
+        ?>
 
-echo("First php commit");
 
-
-
-
-
-
-
-
-?>
+    </body>
+</html>
